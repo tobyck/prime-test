a2046435384e03a104a75486407d1a651f65094c
Toby Connor-Kebbell <84485533+TobyCK@users.noreply.github.com>
2022-02-25 20:06:54 +1300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,7 @@
 import random
 def test(n, k = 40):
+    if n < 2 or n % 2 == 0: return False
+    if n in [2, 3]: return True
     s = n - 1
     for _ in range(k):
         a = random.randrange(2, n - 1)
